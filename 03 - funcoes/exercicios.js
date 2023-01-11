f0b159a4e9b4d3452cbe66ebd98312a25ce9c499
Santhiago <santhiagosantos7@gmail.com>
2023-01-10 22:51:43 -0300

    calculando juros simples com funcoes

---
+++ 03 - funcoes/exercicios.js
@@ -0,0 +1,19 @@
+function escreverMeuNome(nome) {
+  console.log("Meu nome é " + nome);
+}
+
+escreverMeuNome("Santhiago Santos");
+escreverMeuNome("Laura Campos");
+
+
+// Criando função para verificar a maior idade.
+
+function verificarMaiorIdade(idade) {
+  if (idade >= 18) {
+    console.log("Maior de idade");
+  } else {
+    console.log("Menor de idade");
+  }
+}
+
+verificarMaiorIdade(22);
