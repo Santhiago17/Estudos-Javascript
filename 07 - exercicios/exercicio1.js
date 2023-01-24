c0f338cc117b86c60a7234b8b88747088524d208
Santhiago <santhiagosantos7@gmail.com>
2023-01-23 21:00:46 -0300

    programa utilizando funcoes auxiliares

--- 07 - exercicios/exercicio1.js
+++ 07 - exercicios/exercicio1.js
@@ -12,3 +12,15 @@ Saída:
 Recuperação
 
 */
+
+const { gets, print } = require("./funcoes_auxiliares");
+
+const media = gets();
+
+if (media < 5) {
+  print("Reprovado");
+} else if (media >= 5 && media < 7) {
+  print("Recuperação");
+} else {
+  print("Aprovado");
+}
